36ec7212e1496fe1a392d9f5b8222f98b003efad
korkutozcan <korkutozcan@users.noreply.github.com>
2018-02-15 12:18:26 +0300

    Update and rename find_spesific_list.sh to find_large_files.sh

--- find_large_files.sh
+++ find_large_files.sh
@@ -1,20 +1,22 @@
 #/bin/sh
 # Script written by Korkut Ozcan
 #
-# korkutozcan.com (2/9/2018) 
+# korkutozcan.com (2/9/2018)
 #
 name="`uname -n`"_"`/bin/date +%m-%d-%Y_%H%M`"
 echo " "
 echo "*** Write specific directory ***"
 echo " "
+echo "Example /var/log"
 read directory
 echo " "
 echo "Write find to size (size-in-kilebyte)"
 echo "Example 50000(50MB) | 500000(500MB) | 1000000(1GB)"
 read size
-find $directory type f -size +${size}k -exec du -Sh {} + | sort -rh | head -n55 | more
 echo " "
+find ${directory} -type f -size +${size}k -exec ls -lh {} \; | awk '{ print $9 ": " $5 }' | more
 echo "*** Finished Script *** "
-find $directory type f -size +${size}k -exec du -Sh {} + | sort -rh  >> /tmp/$name.txt
+find ${directory} -type f -size +${size}k -exec ls -lh {} \; | awk '{ print $9 ": " $5 }'  >> /tmp/$name.txt
 echo " "
-echo "*** You can find the file /tmp/$name.txt ***" 
+echo "*** You can find the file /tmp/$name.txt ***"
+
